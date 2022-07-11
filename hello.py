40d5729d120195cf196846ce443062a1ec2ea854
topcodemaverick <topcodemavrick@gmail.com>
2022-07-12 01:11:45 +0800

    try to deploy

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flasgger import Swagger
 
 app = Flask(__name__)
+app.debug = True
 swagger = Swagger(app)
 
 @app.route('/v1/img_for_word', methods=['GET'])
@@ -24,3 +25,7 @@ def test_api():
           ans: {"main_color":"red", "main_shape":["00100", "01010", "10001", "00100", "01010"]}
   """
   return jsonify(ans={"main_color":"red", "main_shape":["00100", "01010", "10001", "01010", "00100"]})
+
+if __name__  == '__main__':
+    app.debug = True
+    app.run(host="0.0.0.0",port=5777)
