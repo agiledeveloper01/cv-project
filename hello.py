3863ca1aff5935a38e15b64099fccb4e829218e5
topcodemaverick <topcodemavrick@gmail.com>
2022-07-11 23:05:14 +0800

    first commit

---
+++ hello.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+from flasgger import Swagger
+
+app = Flask(__name__)
+swagger = Swagger(app)
+
+@app.route('/v1/img_for_word', methods=['GET'])
+def test_api():
+  """
+    Get img for word
+    ---
+    tags:
+      - Node APIs
+    produces: application/json,
+    parameters:
+    - name: word
+      in: query
+      type: string
+      required: true
+    responses:
+      200:
+        description: Retrieve image for word
+        examples:
+          ans: {"main_color":"red", "main_shape":["00100", "01010", "10001", "00100", "01010"]}
+  """
+  return jsonify(ans={"main_color":"red", "main_shape":["00100", "01010", "10001", "01010", "00100"]})
